0f6d1de592342fab6eda64da33aff35dad0c5d18
bardos <bardos@bardos.local>
2024-06-04 10:54:22 +0200

    v3.18.1

--- dll_cs_lua_R_fortran_pascal/fortran/tinyfd_main.f90
+++ dll_cs_lua_R_fortran_pascal/fortran/tinyfd_main.f90
@@ -170,7 +170,7 @@
       ________________________________________________________________________
 ! 1) | Install The Windows SDK                                                |
 !    |     http://developer.microsoft.com/en-us/windows/downloads/windows-sdk |
-!    |                            (the user doesn't need to install anything) |
+!    |                          The end user doesn't need to install anythings |
 !    |________________________________________________________________________|
 ! 2) Create a new empty C/C++ project, verify the configuration is for X64.
 ! 3) Add existing files: tinyfiledialogs.c and tinyfiledialogs.h
